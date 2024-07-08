88630f0d4aee926573686c9b0facb55eeeaa3d48
Adeyemi Olaoye <yhemi06@gmail.com>
2024-07-08 22:53:08 +0100

    modified hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Identicon from "react-identicons";
+
+const imgHero =
+  "https://images.cointelegraph.com/images/1434_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDYvNGE4NmNmOWQtODM2Mi00YmVhLThiMzctZDEyODAxNjUxZTE1LmpwZWc=.jpg";
 
 const Hero = () => {
   return (
@@ -15,9 +19,49 @@ const Hero = () => {
         </div>
 
         <div className="flex mt-5">
-          <button className="shadow-xl shadow-black bg-[#e32970] hover:bg-[#bd255f] text-white rounded-full p-2">
+          <button
+            className="shadow-xl shadow-black text-white 
+          bg-[#e32970] hover:bg-[#bd255f] rounded-full p-2"
+          >
             Create NFT
           </button>
+        </div>
+
+        <div className="w-3/4 flex justify-between items-center mt-5">
+          <div className="text-white">
+            <p className="font-bold">123k</p>
+            <small className="text-gray-300">Users</small>
+          </div>
+          <div className="text-white">
+            <p className="font-bold">152k</p>
+            <small className="text-gray-300">Artworks</small>
+          </div>
+          <div className="text-white">
+            <p className="font-bold">200k</p>
+            <small className="text-gray-300">Artists</small>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="shadow-xl shadow-black md:w-2/5 w-full
+      mt-10 md:mt-0 rounded-md overflow-hidden bg-gray-800"
+      >
+        <img
+          className="h-60 w-full object-cover "
+          src={imgHero}
+          alt="Alt Hero"
+        />
+        <div className="flex justify-start items-center p-3">
+          <Identicon
+            className="h-10 w-10 object-contain rounded"
+            string="0x21...784a"
+            size={50}
+          />
+          <div>
+            <p className="text-white">0x21...784a</p>
+            <small>@you</small>
+          </div>
         </div>
       </div>
     </div>
