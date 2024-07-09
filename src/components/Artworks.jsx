d826305daceb2b7d0f88da2b4a75dd521377725b
Adeyemi Olaoye <yhemi06@gmail.com>
2024-07-09 11:45:30 +0100

    create Transactions  and index.jsx components

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -16,6 +16,11 @@ const Artworks = () => {
               <Card key={i} nft={i + 1} />
             ))}
         </div>
+        <div className="text-center my-5">
+          <button className="shadow-lg shadow-black text-white bg-[#e32970] hover:bg-[#bd255f] rounded-full px-1.5 py-2">
+            Load More
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -44,7 +49,7 @@ const Card = ({ nft }) => (
         <small className="text-xs">Current Price</small>
         <p className="text-sm font-semibold">0.34 ETH</p>
       </div>
-      <button className="shadow-lg shadow-black bg-[#e32970] hover:bg-[#bd255f] rounded-full px-1.5 py-1">
+      <button className="shadow-lg shadow-black text-sm bg-[#e32970] hover:bg-[#bd255f] rounded-full px-1.5 py-1">
         View Details
       </button>
     </div>
