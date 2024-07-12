8415f56c09fb90f22a0b6b38eeaf69271f21c02b
Adeyemi Olaoye <yhemi06@gmail.com>
2024-07-12 07:21:38 +0100

    added alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { useGlobalState } from "../store";
 
-const Loading = () => {
-  const [loading] = useGlobalState("loading");
+const Alert = () => {
   return (
     <div
       className={`fixed top-0 left-0  w-screen h-screen flex items-center 
       justify-center bg-black bg-opacity-50 transform transition-transform 
-      duration-300 ${loading.show ? "scale-100" : "scale-0"}`}
+      duration-300 scale-100`}
     >
       <div
         className="bg-[#151c25] shadow-xl shadow-[#e32970] rounded-xl
@@ -16,13 +14,13 @@ const Loading = () => {
         <div className="flex flex-col text-white">
           <div className="flex justify-center items-center">
             <div className="lds-dual-ring scale-50"></div>
-            <p className="text-lg">Processing...</p>
+            <p className="text-lg">Alerting</p>
           </div>
-          <small className="text-center">{loading.msg}</small>
+          <small className="text-center">Alert Message</small>
         </div>
       </div>
     </div>
   );
 };
 
-export default Loading;
+export default Alert;
